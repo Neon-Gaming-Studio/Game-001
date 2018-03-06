6d924d3db0d8dc76ebc650f23e898f46fb9a7d8a
metaljim <james.gilby83@gmail.com>
2018-03-06 21:55:28 +0000

    Rearranged Folders made Pickup Material

--- Game-001/Assets/Prototype/Scripts/HUDCanvas.cs
+++ Game-001/Assets/Prototype/Scripts/HUDCanvas.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 public class HUDCanvas : MonoBehaviour {
 
     public Slider healthBar;
-    float someValue = 0.019283f;
+    
     
  
     void Start() {
 
-        Debug.Log(someValue);
+        
 
     }
 
